650b29f3e41061c9e992a579b254f0cb972e707a
ngankhanh98 <ngankhanh98@gmail.com>
2020-07-22 23:14:09 +0700

    Updates

---
+++ precache-manifest.e36aafde8d53c73f7fbde94be5927273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342876a93f757cc1c3b3e6a593fa8f8f",
+    "url": "/client-ibanking/index.html"
+  },
+  {
+    "revision": "f3d092008d64be83290b",
+    "url": "/client-ibanking/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "e1e9ffd3b940d4ca002e",
+    "url": "/client-ibanking/static/css/main.9adac687.chunk.css"
+  },
+  {
+    "revision": "f3d092008d64be83290b",
+    "url": "/client-ibanking/static/js/2.e23f862f.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/client-ibanking/static/js/2.e23f862f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e9ffd3b940d4ca002e",
+    "url": "/client-ibanking/static/js/main.62a990e9.chunk.js"
+  },
+  {
+    "revision": "5f432ecfb1a6242d2b78",
+    "url": "/client-ibanking/static/js/runtime-main.1d77a5da.js"
+  }
+]);
