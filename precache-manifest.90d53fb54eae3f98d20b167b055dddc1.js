3afd0e28421815d237f84cf8afc9a6eade53acc2
ngankhanh98 <ngankhanh98@gmail.com>
2020-08-19 01:09:18 +0700

    Updates

---
+++ precache-manifest.90d53fb54eae3f98d20b167b055dddc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b74a502e2b9d0c002bf398b1dd2a6fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b30f41d1d1a253114c",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "929feb97c82a5f8e557c",
+    "url": "/static/css/main.9adac687.chunk.css"
+  },
+  {
+    "revision": "11b30f41d1d1a253114c",
+    "url": "/static/js/2.59de0600.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.59de0600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929feb97c82a5f8e557c",
+    "url": "/static/js/main.c69262b4.chunk.js"
+  },
+  {
+    "revision": "e98183dfeaad52d286b1",
+    "url": "/static/js/runtime-main.8ad77837.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/client-ibanking/precache-manifest.3455fd86c43659a0c024535f0132c4b9.js"
+  "/precache-manifest.90d53fb54eae3f98d20b167b055dddc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/client-ibanking/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
