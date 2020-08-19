4a14d1c61222e2f00d7596ecd15616288e011970
ngankhanh98 <ngankhanh98@gmail.com>
2020-08-19 12:53:54 +0700

    🐛 Revert full locahost deploy + fix notif when creating debt

--- src/components/user/debtPage/create_debt_modal.jsx
+++ src/components/user/debtPage/create_debt_modal.jsx
@@ -97,7 +97,7 @@ const CreateDebtForm = ({ visible, onCreate, onCancel, owner, socket }) => {
 
                         (async () => {
                             const ret = await postNotif({
-                                sender: creditor, receiver: payer.beneficiary_account[0], type: "createdebt", amount, description
+                                sender: creditor, receiver: payer.beneficiary_account, type: "createdebt", amount, description
                             })
                             console.log('creditorAccounts[0] :>> ', creditorAccounts[0]);
                             const accountInfo = await getAccountInfo({ account_number: creditorAccounts[0] })
